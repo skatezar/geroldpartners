44ee47a38c07d5290a5bdb8e2ed2e9294ac36f03
laurenz gerold <skatezar@gmail.com>
2026-05-18 09:48:30 +0100

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'geroldpartners.com', protocol: 'https' }
+  config.action_mailer.default_url_options = { host: 'www.geroldpartners.com', protocol: 'https' }
 
   # Postmark configuration for production
   config.action_mailer.delivery_method = :postmark
